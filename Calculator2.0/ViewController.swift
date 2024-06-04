376d344dedbf1908694e0db3932e837012c9837e
Egor Denisenko <lodpsaf@gmail.com>
2024-06-04 15:12:07 +0300

    Initial Commit

---
+++ Calculator2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator2.0
+//
+//  Created by Egor on 04.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
