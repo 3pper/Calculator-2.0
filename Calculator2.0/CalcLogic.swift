040b96d25ea79eba0928325b39c03c6910663518
Egor Denisenko <lodpsaf@gmail.com>
2024-09-21 18:51:14 +0300

    some changes

--- Calculator2.0/CalcLogic.swift
+++ Calculator2.0/CalcLogic.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+struct CalcLogic {
+    
+    private var number: Double?
+    
+    var intCalclulation : (calcMethod: String, n1: Double)?
+    
+    mutating func setNumber(_ number: Double) {
+        self.number = number
+    }
+    
+    mutating func calculation(symbol: String) -> Double? {
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n / 100
+            case "=":
+                return performTwoNumCalculation(n2: n)
+            default:
+                intCalclulation = (calcMethod: symbol, n1: n)
+            }
+            
+        }
+        return nil
+    }
+    
+    private func performTwoNumCalculation(n2: Double) -> Double? {
+        if let n1 = intCalclulation?.n1, let operation = intCalclulation?.calcMethod {
+            
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "÷":
+                return n1 / n2
+            case "x":
+                return n1 * n2
+            default:
+                fatalError("dont work calculation")
+            } 
+        }
+        return nil
+    }
+}
+
+
+/* Bugs
+Нельзя вычислить -5 * 6, т.к. - является вичислительным значением
+*/
--- Calculator2.0/ViewController.swift
+++ Calculator2.0/ViewController.swift
@@ -9,6 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var isFinishTyping: Bool = true
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else { fatalError("Can't convert number")}
+            return number
+        }
+        set {
+            return displayLabel.text = String(newValue)
+        }
+    }
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var calc = CalcLogic()
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        isFinishTyping = true
+        
+        calc.setNumber(displayValue)
+        
+        if let calcNumberber = sender.titleLabel?.text {
+            
+            if let result = calc.calculation(symbol: calcNumberber) {
+                displayValue = result
+            }
+            
+        }
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+
+        if let number = sender.titleLabel?.text {
+            if isFinishTyping {
+                displayLabel.text = number
+                isFinishTyping = false
+            } else {
+                if number == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text! += number
+            }
+            
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
